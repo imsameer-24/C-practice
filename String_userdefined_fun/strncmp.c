f83f9dc0b313da6e6db6c0fd09d01141a81607f4
Sameer Kumar Pattanaik <sameerkumarpattanaik8@gmail.com>
2025-11-17 19:48:07 +0530

    Update strncmp.c

--- String_userdefined_fun/strncmp.c
+++ String_userdefined_fun/strncmp.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-int *my_strncmp(char*,char*,int);
+int my_strncmp(char*,char*,int);
 int main()
 {
 	char s1[20],s2[20];
@@ -9,7 +9,7 @@ int main()
 	scanf("%s%s",s1,s2);
 	printf("Enter the n value\n");
 	scanf("%d",&n);
-	int *r = my_strncmp(s1,s2,n);
+	int r = my_strncmp(s1,s2,n);
 	if(r == 0)
 		printf("strings are equal\n");
 	else if(r > 0)
@@ -17,18 +17,18 @@ int main()
 	else
 		printf("s1 is smaller, s2 is greater\n");
 }
-int *my_stnrcmp(char* s1,char* s2,int n)
+int my_strncmp(char *s1,char *s2,int n)
 {
-	int i;
-	for(i=0;(i<n)&&(s1[i]&&s2[i]);i++)
-	{
-		if(s1[i]!=s2[i])
-			break;
-	}
-	int m=(s1[i]-s2[i]);
-	if(i==n)
-		return 0;
-	else
-		return m;
+    int i;
+    for(i = 0;i < n;i++)
+    {
+        if(s1[i]!= s2[i])      
+            return s1[i]-s2[i];
+
+        if (s1[i] == '\0')
+			return 0;
+    }
+    return 0;
 }
+
 
